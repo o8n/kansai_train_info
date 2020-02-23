3ccf08667a943069ecc807b1383fd89fd2449d25
okamotchan <m.okamotchan@gmail.com>
2020-02-24 02:08:30 +0900

    fix

--- lib/KansaiTrainInfo/client.rb
+++ lib/KansaiTrainInfo/client.rb
@@ -4,23 +4,28 @@ require 'open-uri'
 module KansaiTrainInfo
   class << self
     PAGES = {
-      大阪環状線: [6,2,263]
+      大阪環状線: [4,2,263],
+      近鉄京都線: [6,5,288]
     }.freeze
 
     def get(route_array, url: false)
       messages = []
-        route_array.each do |route|
-          status_xpath = "//*[id='mdAreaMajorLine']/div[#{PAGES[route.to_sym][0]}]/table/tr[#{PAGES[route.to_sym[1]]}]/td[2]"
-          detail_url = "https://transit.yahoo.co.jp/traininfo/detail/#{PAGES[route.to_sym[2]]}/0/"
-          state = kansai_doc/xpath(status_xpath).first.text
-          messages << message(route, state, url, detail_url)
-        end
+
+      route_array.each do |route|
+        status_xpath = "//*[@id='mdAreaMajorLine']/div[#{PAGES[route.to_sym][0]}]/table/tr[#{PAGES[route.to_sym][1]}]/td[2]"
+        detail_url = "https://transit.yahoo.co.jp/traininfo/detail/#{PAGES[route.to_sym][2]}/0/"
+
+        state = kansai_doc.xpath(status_xpath).first.text
+
+        messages << message(route, state, url, detail_url)
+      end
+
       messages
     end
 
     def kansai_doc
-      url = 'https://transit.yahoo.co.jp/traininfo/area/6/'
       charset = nil
+      url = 'https://transit.yahoo.co.jp/traininfo/area/6/'
         html = open(url) do |f|
           charset = f.charset
           f.read
@@ -35,13 +40,17 @@ module KansaiTrainInfo
         f.read
       end
       detail_doc = Nokogiri::HTML.parse(detail_html, nil, charset)
-      detail_doc.xpath('//*[id="mdServiceStatus"]/dl/dd/p').first.text
+      detail_doc.xpath('//*[@id="mdServiceStatus"]/dl/dd/p').first.text
     end
 
     def message(route, state, url_option, detail_url)
       state.slice!('[○]')
       state.slice!('[!]')
-      return "#{route}は#{state}です" if state == '平常運転'
+
+        if state == '平常運転'
+          puts "#{route}は#{state}です"
+        end
+
       message = case state
                 when '運転状況'
                   "#{route}は#{state}に変更があります。"
@@ -50,6 +59,7 @@ module KansaiTrainInfo
                 when '運転見合わせ'
                   "#{route}は#{state}しています。"
                 end
+
       if url_option
         message + "\n" + description(detail_url) + "\n" + detail_url
       else
